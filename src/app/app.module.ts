214eabbae8d1b9d178f6e1acef65f679c45d9883
pepsamo <giuseppegiovanni.amoruso@gmail.com>
2019-11-10 19:10:20 +0100

    Change manifest file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
-import {MatTabGroup} from "@angular/material/tabs";
+import {MatTabsModule} from "@angular/material/tabs";
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
@@ -18,7 +18,7 @@ import { environment } from '../environments/environment';
     BrowserModule,
     NgbModule,
     BrowserAnimationsModule,
-    MatTabGroup,
+    MatTabsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
