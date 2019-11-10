32794a7fb892429f58d3b831527c1f2c072ab9b4
pepsamo <giuseppegiovanni.amoruso@gmail.com>
2019-11-10 18:48:21 +0100

    Add angiular pwa package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import {MatTabGroup} from "@angular/material/tabs";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import {MatTabGroup} from "@angular/material/tabs";
     BrowserModule,
     NgbModule,
     BrowserAnimationsModule,
-    MatTabGroup
+    MatTabGroup,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
