db70781af646178ae84bf171572988ce1449948d
pepsamo <giuseppegiovanni.amoruso@gmail.com>
2019-11-21 21:11:44 +0100

    Create some sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,36 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { RouterModule, Routes } from "@angular/router";
 import { environment } from '../environments/environment';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { AboutMeComponent } from './sections/about-me/about-me.component';
+import { SkillsComponent } from './sections/skills/skills.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+  redirectTo: '/',
+  pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutMeComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
     BrowserAnimationsModule,
     FontAwesomeModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
